0c1bfd3a2bb0c93b6b261a50c6af46db176bb28f
jwhityoung <jwhityoung@gmail.com>
2020-06-25 20:08:43 -0500

    almost done

--- script.js
+++ script.js
@@ -4,33 +4,86 @@
 $("#currentDay").text(moment().format('MMMM Do, YYYY'));
 
 //Current time 
-var today = new Date();
-var time = today.getHours() + today.getMinutes();
-console.log(time);
+var currentHour = (moment().format('HH'));
+console.log(currentHour);
 
-let currentTime = time;
-if (time > )
 
-   
+// //Dynamic background color 
+// function dynamicColor9() {
+
+//     // $(".text-area").each(function () {
+//     //     var block = $(this).attr("id").split("-")[0];
+//     //     console.log(block)
+
+//         if (currentHour > block) {
+//             console.log("past");
+//         //   $(".text-area").css("background-color", "#78C2AD");
+            
+//         }
+//         else if (currentHour < block) {
+//             // $(".text-area").css("background-color", "red");
+            
+//         }
+//         else if (currentHour = block) {
+//             console.log("future");
+//         //   $(".text-area").css("background-color", "#F3969A");
+
+//         }
+//     // });
+// }
+
+// dynamicColor9();
+
+// //Dynamic background color 
+// function dynamicColor17() {
+
+//     // $(".text-area").each(function () {
+//     //     var block = $(this).attr("id").split("-")[0];
+//     //     console.log(block)
+// var blockFive = 23;
+//         if (currentHour > blockFive) {
+//             console.log("past");
+//             $("#17-form").css("background-color", "#78C2AD");
+            
+//         }
+//         else if (currentHour < blockFive) {
+//             $("#17-form").css("background-color", "red");
+            
+//         }
+//         else if (currentHour == blockFive) {
+//             console.log("future");
+//             $("#17-form").css("background-color", "#F3969A");
+
+//         }
+//     // });
+// }
+
+// dynamicColor17();
+
 
 //9am
 var nineSave = $("#9am-button");
-var nineForm = $("#9am-form");
+var nineForm = $("#9-form");
+var block = (9);
+
+
 nineForm.val(localStorage.getItem("nineEntry"));
 
-nineSave.on("click", function() {
+nineSave.on("click", function () {
     nineForm.text(nineForm.val());
     console.log(nineForm.val());
     var nineEntry = nineForm.val();
     localStorage.setItem("nineEntry", nineEntry);
 });
 
+
+
 //10am
 var tenSave = $("#10am-button");
-var tenForm = $("#10am-form");
+var tenForm = $("#10-form");
 tenForm.val(localStorage.getItem("tenEntry"));
 
-tenSave.on("click", function() {
+tenSave.on("click", function () {
     tenForm.text(tenForm.val());
     console.log(tenForm.val());
     var tenEntry = tenForm.val();
@@ -39,10 +92,10 @@ tenSave.on("click", function() {
 
 //11am
 var elevenSave = $("#11am-button");
-var elevenForm = $("#11am-form");
+var elevenForm = $("#11-form");
 elevenForm.val(localStorage.getItem("elevenEntry"));
 
-elevenSave.on("click", function() {
+elevenSave.on("click", function () {
     elevenForm.text(elevenForm.val());
     console.log(elevenForm.val());
     var elevenEntry = elevenForm.val();
@@ -51,10 +104,10 @@ elevenSave.on("click", function() {
 
 //12pm
 var noonSave = $("#12pm-button");
-var noonForm = $("#12pm-form");
+var noonForm = $("#12-form");
 noonForm.val(localStorage.getItem("noonEntry"));
 
-noonSave.on("click", function() {
+noonSave.on("click", function () {
     noonForm.text(noonForm.val());
     console.log(noonForm.val());
     var noonEntry = noonForm.val();
@@ -63,10 +116,10 @@ noonSave.on("click", function() {
 
 //1pm
 var oneSave = $("#1pm-button");
-var oneForm = $("#1pm-form");
+var oneForm = $("#13-form");
 oneForm.val(localStorage.getItem("oneEntry"));
 
-oneSave.on("click", function() {
+oneSave.on("click", function () {
     oneForm.text(oneForm.val());
     console.log(oneForm.val());
     var oneEntry = oneForm.val();
@@ -76,10 +129,10 @@ oneSave.on("click", function() {
 
 //2pm
 var twoSave = $("#2pm-button");
-var twoForm = $("#2pm-form");
+var twoForm = $("#14-form");
 twoForm.val(localStorage.getItem("twoEntry"));
 
-twoSave.on("click", function() {
+twoSave.on("click", function () {
     twoForm.text(twoForm.val());
     console.log(twoForm.val());
     var twoEntry = twoForm.val();
@@ -88,10 +141,10 @@ twoSave.on("click", function() {
 
 //3pm
 var threeSave = $("#3pm-button");
-var threeForm = $("#3pm-form");
+var threeForm = $("#15-form");
 threeForm.val(localStorage.getItem("threeEntry"));
 
-threeSave.on("click", function() {
+threeSave.on("click", function () {
     threeForm.text(threeForm.val());
     console.log(threeForm.val());
     var threeEntry = threeForm.val();
@@ -100,10 +153,10 @@ threeSave.on("click", function() {
 
 //4pm
 var fourSave = $("#4pm-button");
-var fourForm = $("#4pm-form");
+var fourForm = $("#16-form");
 fourForm.val(localStorage.getItem("fourEntry"));
 
-fourSave.on("click", function() {
+fourSave.on("click", function () {
     fourForm.text(fourForm.val());
     console.log(fourForm.val());
     var fourEntry = fourForm.val();
@@ -112,10 +165,10 @@ fourSave.on("click", function() {
 
 //5pm
 var fiveSave = $("#5pm-button");
-var fiveForm = $("#5pm-form");
+var fiveForm = $("#17-form");
 fiveForm.val(localStorage.getItem("fiveEntry"));
 
-fiveSave.on("click", function() {
+fiveSave.on("click", function () {
     fiveForm.text(fiveForm.val());
     console.log(fiveForm.val());
     var fiveEntry = fiveForm.val();
